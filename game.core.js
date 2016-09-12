c05f8778ba91a12b68db2bf1df6ea32f8eacd6e5
Derek Feddon <dfeddon@gmail.com>
2016-09-12 14:03:38 -0400

    Working on iOS wrapper (calling js functions).

--- game.core.js
+++ game.core.js
@@ -2106,9 +2106,7 @@ document.derek = function()
     var game = document.getElementById('viewport').ownerDocument.defaultView.game;
 
     var evt = document.createEvent("KeyboardEvent");
-    /*evt.initKeyEvent ("keypress", true, true, window,
-                    0, 0, 0, 0,
-                    38, 0);*/
+    evt.initKeyEvent("keypress", true, true, window, 0, 0, 0, 0, 38, 0);
     //document.dispatchEvent(evt);
 
     //alert(game.players.self.mp);
